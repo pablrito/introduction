58c12cb08fcabda51591a350a20c2a70eb4b2442
pablrito <pablo.rito@tele2.com>
2020-03-09 17:29:24 +0100

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import tele2 from './asc6h-z2cnj.svg';
+import Metric from './components/Metric'
+import Health from './components/Health'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Teamtau is back , terminator
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="top-row px-4">
+                <div className="col text-center">
+                </div>
+            </div>
+            <div className="content px-4">
+                <div className="row mb-3">
+				    <div className="col-10"></div>
+                    <div className="col-2 float-right"> </div>
+                </div>
+                <div className="row">
+					<div class="sticky-title">
+						<p>restrictions connected </p>
+					</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
